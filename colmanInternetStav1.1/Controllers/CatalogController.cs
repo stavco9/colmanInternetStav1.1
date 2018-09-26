8c336441e90888a3e37f5ca9586b79e16b6d3d69
Daniel <Daniel@LAPTOP-8DS83TB9>
2018-09-26 20:21:58 +0300

    add Catalog

---
+++ colmanInternetStav1.1/Controllers/CatalogController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using colmanInternetStav1._1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace colmanInternetStav1._1.Controllers
+{
+    public class CatalogController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (Account.isLoggedIn(User))
+            {
+                UsersController addNewUser = new UsersController(new ColmanInternetiotContext());
+
+                Dictionary<string, string> userDetails = Account.getDetails(User);
+
+                var newUserToDB = new Users { NameId = userDetails["nameid"], Email = userDetails["emailaddress"], FName = userDetails["firstname"], LName = userDetails["lastname"], Name = userDetails["fullname"], Gender = userDetails["gender"], IsAdmin = false };
+
+                addNewUser.Create(newUserToDB);
+            }
+
+            return View();
+        }
+    }
+}
